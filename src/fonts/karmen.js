48d18ebba4e97127039c59cae4d9b11cbc31f59f
Abhishek Anand <abhishek.a23@iiits.in>
2026-07-06 01:47:02 +0530

    merge error fix

--- src/fonts/karmen.js
+++ src/fonts/karmen.js
@@ -2,7 +2,7 @@
 import localFont from 'next/font/local'
 
 export const karmen = localFont({
-  src: '../../public/fonts/Karmen.otf',
+  src: '../../public/fonts/KARMEN.otf',
   variable: '--font-karmen',
   display: 'swap',
 })
